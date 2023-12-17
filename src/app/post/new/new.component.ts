3cf5bd764ba3625a19ca3f1508cd49987ad2607d
JtQuentin <quentin.jolivet@pm.me>
2023-12-17 19:53:12 +0100

    fix new post

--- src/app/post/new/new.component.ts
+++ src/app/post/new/new.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { postLengthValidator } from '../../@shared/validators';
 import { PostService } from '../post.service';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-new',
@@ -11,7 +12,7 @@ import { PostService } from '../post.service';
 export class NewComponent {
   postForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private postService: PostService) {
+  constructor(private fb: FormBuilder, private postService: PostService, private authService: AuthService) {
     this.postForm = this.fb.group({
       postContent: ['', [Validators.required, postLengthValidator()]],
     });
@@ -20,20 +21,26 @@ export class NewComponent {
   onSubmit() {
     const postContentControl = this.postForm.get('postContent');
 
-  if (postContentControl?.value) {
-    const postContent = postContentControl.value;
+    if (postContentControl?.value) {
+      const postContent = postContentControl.value;
+      const userId = this.authService.user?.id;
+      const createdAt = new Date();
 
-    this.postService.createPost({ content: postContent }).subscribe(
-      (response: any) => {
-        console.log('Post créé avec succès', response);
-
-        this.postForm.reset();
-      },
-      (error: any) => {
-        console.error('Erreur lors de la création du post', error);
+      if (userId) {
+        this.postService.createPost({
+          content: postContent,
+          userId: userId,
+          createdAt: createdAt.toISOString()
+        }).subscribe(
+          (response: any) => {
+            console.log('Post créé avec succès', response);
+            this.postForm.reset();
+          },
+          (error: any) => {
+            console.error('Erreur lors de la création du post', error);
+          }
+        );
       }
-    );
+    }
   }
-  }
-
 }
